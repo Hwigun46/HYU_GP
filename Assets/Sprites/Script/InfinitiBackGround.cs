1929550f8380eb65991ec87b7d94ffecf3921f88
yunsful <bailey05280@naver.com>
2025-05-26 17:57:44 +0900

    feat: 추락시 배경 초기화 및 6스테이지 수정

--- Assets/Sprites/Script/InfinitiBackGround.cs
+++ Assets/Sprites/Script/InfinitiBackGround.cs
@@ -5,6 +5,8 @@ public class InfiniteBackground : MonoBehaviour
     public Transform player;
     public Transform bg1, bg2;    // 배경 오브젝트
     private float backgroundWidth;
+    private float halfWidth;            // 절반 너비
+    private Vector3 startPosition;
 
     void Start()
     {
@@ -12,27 +14,37 @@ public class InfiniteBackground : MonoBehaviour
         SpriteRenderer sr = bg1.GetComponent<SpriteRenderer>();
         // 로컬 스케일을 곱해서 실제 월드 단위 크기 계산
         backgroundWidth = sr.bounds.size.x;
+        halfWidth = backgroundWidth / 2f;
         Debug.Log("배경 너비 (월드 단위): " + backgroundWidth);
+        startPosition = bg1.position;
     }
     void Update()
     {
+        if (player.position.x > bg1.position.x + halfWidth ||
+            player.position.x < bg1.position.x - halfWidth)
+        {
+            // bg1을 초기 위치로 되돌림
+            bg1.position = startPosition;
+        }
         // 플레이어가 bg1의 오른쪽 경계를 넘었을 때
-        if (player.position.x < bg1.position.x && player.position.x > (bg1.position.x - backgroundWidth / 2f))
+        if (player.position.x < bg1.position.x &&
+        player.position.x > bg1.position.x - halfWidth)
         {
             // bg1을 bg2의 오른쪽으로 옮기고 swap
             bg2.position = new Vector3(bg1.position.x - backgroundWidth, bg1.position.y, bg1.position.z);
         }
         // 플레이어가 bg2의 왼쪽 경계를 넘었을 때
-        else if (player.position.x < (bg1.position.x + backgroundWidth / 2f) && player.position.x > bg1.position.x)
+        else if (player.position.x < bg1.position.x + halfWidth &&
+        player.position.x > bg1.position.x)
         {
             // bg2를 bg1의 왼쪽으로 옮기고 swap
             bg2.position = new Vector3(bg1.position.x + backgroundWidth, bg2.position.y, bg2.position.z);
         }
-        else if (player.position.x < bg1.position.x - backgroundWidth / 2f)
+        else if (player.position.x < bg1.position.x - halfWidth)
         {
             SwapBackgrounds();
         }
-        else if (player.position.x > bg1.position.x + backgroundWidth / 2f)
+        else if (player.position.x > bg1.position.x + halfWidth)
         {
             SwapBackgrounds();
         }
